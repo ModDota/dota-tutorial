85e0bed7826dbab2d882df9790b5fa3d8c53d177
Perry van Wesel <Perryvw@users.noreply.github.com>
2021-02-21 13:13:35 +0100

    Initial commit

---
+++ game/scripts/vscripts/addon_game_mode.ts
@@ -0,0 +1,11 @@
+import "./lib/timers";
+import { GameMode } from "./GameMode";
+
+Object.assign(getfenv(), {
+    Activate: GameMode.Activate,
+    Precache: GameMode.Precache,
+});
+
+if (GameRules.Addon) {
+    GameRules.Addon.Reload();
+}
---
+++ game/scripts/vscripts/modifiers/modifier_panic.ts
@@ -0,0 +1,43 @@
+import { BaseModifier, registerModifier } from "../lib/dota_ts_adapter";
+
+// Base speed modifier -- Could be moved to a separate file
+class ModifierSpeed extends BaseModifier {
+    // Declare functions
+    DeclareFunctions(): modifierfunction[] {
+        return [modifierfunction.MODIFIER_PROPERTY_MOVESPEED_ABSOLUTE];
+    }
+
+    GetModifierMoveSpeed_Absolute(): number {
+        return 300;
+    }
+}
+
+@registerModifier()
+export class modifier_panic extends ModifierSpeed {
+    // Set state
+    CheckState(): Partial<Record<modifierstate, boolean>> {
+        return {
+            [modifierstate.MODIFIER_STATE_COMMAND_RESTRICTED]: true,
+        };
+    }
+
+    // Override speed given by Modifier_Speed
+    GetModifierMoveSpeed_Absolute(): number {
+        return 540;
+    }
+
+    // Run when modifier instance is created
+    OnCreated(): void {
+        if (IsServer()) {
+            // Think every 0.3 seconds
+            this.StartIntervalThink(0.3);
+        }
+    }
+
+    // Called when intervalThink is triggered
+    OnIntervalThink(): void {
+        const parent = this.GetParent();
+
+        parent.MoveToPosition((parent.GetAbsOrigin() + RandomVector(400)) as Vector);
+    }
+}
