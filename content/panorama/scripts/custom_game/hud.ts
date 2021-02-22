7f324d5549ce7266f3424eb71fa7fe48360d0bfb
Perryvw <perry.wesel@gmail.com>
2021-02-22 21:06:22 +0100

    Initial janky panorama dev panel

--- content/panorama/scripts/custom_game/hud.ts
+++ content/panorama/scripts/custom_game/hud.ts
@@ -1,8 +1,3 @@
 $.Msg("Hud panorama loaded");
 
-// TODO: Remove later, this is just an example
-GameEvents.Subscribe("my_custom_event", event => {
-    $.Msg("Received custom event", event.foo, event.bar);
-});
-
 GameEvents.SendCustomGameEventToServer<{}>("ui_loaded", {});
