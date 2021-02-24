9ef5d3539b1702c1f54669d46a51edcf472ff01b
W <wouter.zegers@gmail.com>
2021-02-24 22:03:14 +0100

    Added casting section

--- game/scripts/vscripts/GameMode.ts
+++ game/scripts/vscripts/GameMode.ts
@@ -1,5 +1,5 @@
 import { reloadable } from "./lib/tstl-utils";
-import { section01, section02, section03, section_levelling } from "./Sections/index";
+import { section01, section02, section03, section_casting, section_levelling } from "./Sections/index";
 import * as tut from "./Tutorial/Core";
 import { findAllPlayersID, getPlayerHero } from "./util";
 
@@ -14,7 +14,7 @@ export class GameMode {
     Game: CDOTABaseGameMode = GameRules.GetGameModeEntity();
     canPlayerHeroEarnXP = false;
 
-    private tutorial = new tut.Tutorial([section01, section02, section03, section_levelling]);
+    private tutorial = new tut.Tutorial([section01, section02, section03, section_levelling, section_casting]);
     playerHero?: CDOTA_BaseNPC_Hero;
 
     public static Precache(this: void, context: CScriptPrecacheContext) {
--- game/scripts/vscripts/Sections/index.ts
+++ game/scripts/vscripts/Sections/index.ts
@@ -3,3 +3,5 @@ export * from "./Section_levelling"
 export * from "./Section0"
 export * from "./Section2"
 export * from "./Section3"
+
+export * from "./Section_casting"
