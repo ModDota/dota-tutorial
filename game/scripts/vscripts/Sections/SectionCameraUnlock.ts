0d3ac239f271b4574bc77e13a70fc82eda34e6af
Robin Kahlow <robinkahlow@web.de>
2021-02-25 01:25:54 +0000

    impl attack sunsfan golem

--- game/scripts/vscripts/Sections/SectionCameraUnlock.ts
+++ game/scripts/vscripts/Sections/SectionCameraUnlock.ts
@@ -18,6 +18,8 @@ const onStart = (complete: () => void) => {
         error("Could not find fountain (ent_dota_fountain_good)")
     }
 
+    let sunsfanAttackableTime: number | undefined = undefined
+
     graph = tg.seq(
         tg.immediate(_ => print("Pre camera movement")),
         tg.waitForCameraMovement(),
@@ -34,8 +36,24 @@ const onStart = (complete: () => void) => {
         tg.wait(1),
         // TODO: Make sunsfan golem attackable
         tg.playGlobalSound("abaddon_abad_spawn_01"), // why health bar over head
-        // TODO: Play sound if sunsfan wasnt killed within 10s
-        // TODO: Wait for player to attack sunsfan
+        tg.immediate(_ => sunsfanAttackableTime = GameRules.GetGameTime()),
+        tg.completeOnCheck(_ => {
+            const golem = Entities.FindAllByClassname("npc_mud_golem_sunsfan")[0]
+            if (!golem) {
+                error("Could not find SUNSfan golem")
+            }
+
+            const attacked = golem.GetHealth() < golem.GetMaxHealth()
+
+            // Play a sound if the player didn't attack SUNSfan golem for 10 seconds
+            if (!attacked && sunsfanAttackableTime && GameRules.GetGameTime() - sunsfanAttackableTime > 10) {
+                EmitGlobalSound("abaddon_abad_spawn_01") // come on / attack the man
+                sunsfanAttackableTime = undefined
+            }
+
+            return attacked
+        }, 0.2),
+        tg.immediate(_ => sunsfanAttackableTime = undefined),
         tg.playGlobalSound("abaddon_abad_spawn_01", true), // what are you doing
         tg.immediate(context => playerHero.HeroLevelUp(true)),
         tg.wait(1)
