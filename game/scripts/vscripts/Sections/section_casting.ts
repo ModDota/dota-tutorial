c5df75c6ab36e0c365860495c0a0f609001182ee
W <wouter.zegers@gmail.com>
2021-02-25 20:24:01 +0100

    Fixed formatting

--- game/scripts/vscripts/Sections/section_casting.ts
+++ game/scripts/vscripts/Sections/section_casting.ts
@@ -1,12 +1,12 @@
-import * as tg from "../TutorialGraph/index"
-import * as tut from "../Tutorial/Core"
-import { getPlayerHero } from "../util"
+import * as tg from "../TutorialGraph/index";
+import * as tut from "../Tutorial/Core";
+import { getPlayerHero } from "../util";
 
-let graph: tg.TutorialStep | undefined = undefined
-let graphContext: tg.TutorialContext | undefined = undefined
+let graph: tg.TutorialStep | undefined = undefined;
+let graphContext: tg.TutorialContext | undefined = undefined;
 
 const start = (complete: () => void) => {
-    print("Started section casting")
+    print("Started section casting");
     // Assuming the player knows camera control.
 
     // Sequence:
@@ -19,19 +19,16 @@ const start = (complete: () => void) => {
 
     const hero = getPlayerHero();
     const abilityName = "dragon_knight_breathe_fire";
-    
+
     if (!hero || hero.IsNull()) {
         error("No hero found");
     }
 
-    
-
-    for (let i = 0; i < DOTA_MAX_ABILITIES -1; i++) {
+    for (let i = 0; i < DOTA_MAX_ABILITIES - 1; i++) {
         const a = hero.GetAbilityByIndex(i);
         if (a) {
             hero.RemoveAbility(a.GetAbilityName());
         }
-        
     }
 
     if (!hero.HasAbility(abilityName)) {
@@ -39,40 +36,64 @@ const start = (complete: () => void) => {
     }
     const ability = hero.FindAbilityByName(abilityName)!;
     ability.UpgradeAbility(true);
-    
+
     ability.SetUpgradeRecommended(false);
     ability.SetUpgradeRecommended(true);
 
     // TODO FIX location
     const heroLoc = <Vector>(hero.GetAbsOrigin() + RandomVector(500));
-    const pugna = CreateUnitByName("npc_dota_hero_pugna",heroLoc,true,undefined,undefined,DotaTeam.BADGUYS)
+    const pugna = CreateUnitByName(
+        "npc_dota_hero_pugna",
+        heroLoc,
+        true,
+        undefined,
+        undefined,
+        DotaTeam.BADGUYS
+    );
     pugna.SetHealth(1);
-    pugna.AddNewModifier(undefined,undefined,"modifier_tutorial_disable_healing",{});
-    const disarmModifier = pugna.AddNewModifier(pugna,pugna.FindAbilityByName("modifier_pugna_decrepify"),"modifier_pugna_decrepify", {})
+    pugna.AddNewModifier(
+        undefined,
+        undefined,
+        "modifier_tutorial_disable_healing",
+        {}
+    );
+    const disarmModifier = pugna.AddNewModifier(
+        pugna,
+        pugna.FindAbilityByName("modifier_pugna_decrepify"),
+        "modifier_pugna_decrepify",
+        {}
+    );
 
     graph = tg.seq(
-        tg.completeOnCheck(()=> !ability.IsCooldownReady() && !pugna.IsAlive(),.1),
-        
-    )
+        tg.completeOnCheck(
+            () => !ability.IsCooldownReady() && !pugna.IsAlive(),
+            0.1
+        )
+    );
 
-    graphContext = {}
+    graphContext = {};
 
     graph.start(graphContext, () => {
-        print("Section casting was completed")
-        complete()
-    })
-}
+        print("Section casting was completed");
+        complete();
+    });
+};
 
 const resetState = () => {
     // TODO: Make sure DK exists at spawn and other stuff (yea stuff...)
-}
+};
 
 const stop = () => {
     if (graph) {
-        graph.stop(graphContext ?? {})
-        graph = undefined
-        graphContext = undefined
+        graph.stop(graphContext ?? {});
+        graph = undefined;
+        graphContext = undefined;
     }
-}
+};
 
-export const section_casting = new tut.FunctionalSection("Section_Casting", start, resetState, stop)
+export const section_casting = new tut.FunctionalSection(
+    "Section_Casting",
+    start,
+    resetState,
+    stop
+);
