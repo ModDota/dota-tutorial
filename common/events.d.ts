dc8e5fa1ca49b308b41918d429511b893fcfa0c7
VicFrank <matthewshattil@gmail.com>
2021-02-26 17:14:51 -0600

    Added client side camera movement (#55)
    * Added client side camera movement
    
    * Responding to pull request comments
    
    * fuck merges

--- common/events.d.ts
+++ common/events.d.ts
@@ -6,9 +6,18 @@ interface SectionStartedEvent {
     section: SectionName;
 }
 
+interface MoveCameraEvent {
+    cameraTargetX?: number;
+    cameraTargetY?: number;
+    cameraTargetZ?: number;
+    unitTargetEntIndex?: EntityIndex;
+    lerp: number;
+}
+
 interface CustomGameEventDeclarations {
     section_started: SectionStartedEvent;
     skip_to_section: SkipToSectionEvent;
+    move_camera: MoveCameraEvent;
     ui_loaded: {};
     detect_camera_movement: {};
     camera_movement_detected: {};
