8f476bace7083237fc3a1610518c158c9a1b91e4
Robin Kahlow <robinkahlow@web.de>
2021-02-28 21:59:43 +0000

    add goals to template

--- game/scripts/vscripts/Sections/Chapter1/SectionTemplate.ts
+++ game/scripts/vscripts/Sections/Chapter1/SectionTemplate.ts
@@ -13,15 +13,55 @@ const requiredState: RequiredState = {
 };
 
 /**
+ * Keys used for the tutorial graph context in this section.
+ */
+enum ContextKey {
+    Dummy
+}
+
+enum GoalState {
+    Started,
+    Completed
+}
+
+/**
+ * Return a list of goals to display depending on which parts we have started and completed.
+ * @param context Context of the tutorial graph which can store the goals' states.
+ */
+const getGoals = (context: tg.TutorialContext) => {
+    const isGoalStarted = (key: ContextKey) => context[key] === GoalState.Started || context[key] === GoalState.Completed
+    const isGoalCompleted = (key: ContextKey) => context[key] === GoalState.Completed
+
+    const goals: Goal[] = []
+    const addGoal = (key: ContextKey, text: string) => {
+        if (isGoalStarted(key)) {
+            goals.push({ text: text, completed: isGoalCompleted(key) })
+        }
+    }
+
+    addGoal(ContextKey.Dummy, "Do something.")
+
+    return goals
+}
+
+/**
  * Called when the section is started and should contain the main logic.
  * @param complete Call this function to complete the section.
  */
 function onStart(complete: () => void) {
     print("Starting", sectionName);
 
-    graph = tg.seq([
-        tg.immediate(context => print("Hello world"))
-    ]);
+    graph = tg.forkAny([
+        tg.trackGoals(getGoals),
+        tg.seq([
+            tg.wait(1),
+            tg.immediate(context => context[ContextKey.Dummy] = GoalState.Started),
+            tg.immediate(context => print("Hello world")),
+            tg.wait(5),
+            tg.immediate(context => context[ContextKey.Dummy] = GoalState.Completed),
+            tg.wait(5),
+        ])
+    ])
 
     graph.start(GameRules.Addon.context, () => {
         print("Completed", sectionName);
