29419ccbe6b60b7ce88dec9a7ce2d7e689b7e0dc
VicFrank <matthewshattil@gmail.com>
2021-02-28 22:46:43 -0600

    Added Dialog Log

--- common/general.d.ts
+++ common/general.d.ts
@@ -18,9 +18,9 @@ declare const enum CustomNpcKeys {
 }
 
 type Goal = {
-    text: string
-    completed: boolean
-}
+    text: string;
+    completed: boolean;
+};
 
 interface DialogData {
     text: string;
