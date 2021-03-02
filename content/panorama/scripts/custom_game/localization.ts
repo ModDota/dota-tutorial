366fb38b401f891ebed5fdb55a16e576af869156
Robin Kahlow <robinkahlow@web.de>
2021-03-02 23:57:32 +0000

    add localizeInline to panorama, use it for quests panel

---
+++ content/panorama/scripts/custom_game/localization.ts
@@ -0,0 +1,15 @@
+const localizationRegex = /#\S+/g; // '#' followed by one or more non-whitespaces
+
+const localizeInline = (text: string): string => text.includes("#") ? text.replace(localizationRegex, match => $.Localize(match)) : $.Localize(text)
+
+interface CustomUIConfig {
+    /**
+     * Replaces occurences of localizable keys starting with '#' within the string and returns the result.
+     * If the string does not contain '#' uses the normal localization function.
+     * Example: "Text Text #Token1 Text #Token2" => "Text Text Token one Text Token two"
+     * @param text String within which we want to replace localization keys.
+     */
+    localizeInline: typeof localizeInline;
+}
+
+GameUI.CustomUIConfig().localizeInline = localizeInline;
