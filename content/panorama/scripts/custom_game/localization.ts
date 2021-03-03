6cf50ca02d3b1484d9a6a90a651a81610bfc5ed4
Robin Kahlow <robinkahlow@web.de>
2021-03-03 00:06:31 +0000

    add note about loc token regex

--- content/panorama/scripts/custom_game/localization.ts
+++ content/panorama/scripts/custom_game/localization.ts
@@ -1,4 +1,4 @@
-const localizationRegex = /#[a-zA-Z0-9_-]+/g; // '#' followed by one or more non-whitespaces
+const localizationRegex = /#[a-zA-Z0-9_-]+/g; // '#' followed by one or more letters/digits/underscores/hyphens
 
 const localizeInline = (text: string): string => text.includes("#") ? text.replace(localizationRegex, match => $.Localize(match)) : $.Localize(text)
 
@@ -6,6 +6,7 @@ interface CustomUIConfig {
     /**
      * Replaces occurences of localizable keys starting with '#' within the string and returns the result.
      * If the string does not contain '#' uses the normal localization function.
+     * The localization tokens can be composed of letters, digits, underscores and hyphens.
      * Example: "Text Text #Token1 Text #Token2" => "Text Text Token one Text Token two"
      * @param text String within which we want to replace localization keys.
      */
