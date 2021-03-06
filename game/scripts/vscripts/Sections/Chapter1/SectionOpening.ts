c4c03de9f20ec89595629e32d09d0a10d93a3058
Robin Kahlow <robinkahlow@web.de>
2021-03-06 17:41:02 +0000

    Polish Chapter 1 (#147)
    * polish chapter 1
    
    * reset bought tango state on section start
    
    * formatting

--- game/scripts/vscripts/Sections/Chapter1/SectionOpening.ts
+++ game/scripts/vscripts/Sections/Chapter1/SectionOpening.ts
@@ -2,44 +2,35 @@ import * as tg from "../../TutorialGraph/index"
 import * as tut from "../../Tutorial/Core"
 import { findRealPlayerID, getPlayerHero } from "../../util"
 import { RequiredState } from "../../Tutorial/RequiredState"
+import { slacksFountainLocation, slacksInitialLocation, sunsfanFountainLocation } from "./Shared"
 
 let graph: tg.TutorialStep | undefined = undefined
 let canPlayerIssueOrders = true;
 const requiredState: RequiredState = {
+    requireSlacksGolem: true,
+    requireSunsfanGolem: true,
+    slacksLocation: slacksInitialLocation,
+    sunsFanLocation: sunsfanFountainLocation,
     requireFountainTrees: true,
 }
 
 const onStart = (complete: () => void) => {
     const playerHero = getPlayerHero();
     if (!playerHero) error("Could not find the player's hero.");
-    const mudGolemMeetPosition = playerHero.GetAbsOrigin().__add(Vector(300, 800, 0))
 
     graph = tg.seq([
         tg.immediate(() => canPlayerIssueOrders = false),
         tg.setCameraTarget(() => playerHero),
-        tg.spawnUnit(CustomNpcKeys.SlacksMudGolem,
-            playerHero.GetAbsOrigin().__add(Vector(0, 1500, 0)),
-            DotaTeam.GOODGUYS,
-            CustomNpcKeys.SlacksMudGolem
-        ),
-        tg.spawnUnit(CustomNpcKeys.SunsFanMudGolem,
-            playerHero.GetAbsOrigin().__add(Vector(1500, 500, 0)),
-            DotaTeam.GOODGUYS,
-            CustomNpcKeys.SunsFanMudGolem
-        ),
         tg.fork([
             tg.seq([
-                tg.moveUnit(context => context[CustomNpcKeys.SlacksMudGolem], mudGolemMeetPosition),
+                tg.moveUnit(context => context[CustomNpcKeys.SlacksMudGolem], slacksFountainLocation),
                 tg.faceTowards(context => context[CustomNpcKeys.SlacksMudGolem], playerHero.GetAbsOrigin()),
             ]),
+            tg.faceTowards(context => context[CustomNpcKeys.SunsFanMudGolem], playerHero.GetAbsOrigin()),
             tg.seq([
-                tg.moveUnit(context => context[CustomNpcKeys.SunsFanMudGolem], mudGolemMeetPosition.__add(Vector(150, -150, 0))),
-                tg.faceTowards(context => context[CustomNpcKeys.SunsFanMudGolem], playerHero.GetAbsOrigin()),
-            ]),
-            tg.seq([
-                tg.setCameraTarget(context => context[CustomNpcKeys.SlacksMudGolem]),
-                tg.wait(3),
                 tg.setCameraTarget(context => context[CustomNpcKeys.SunsFanMudGolem]),
+                tg.wait(2),
+                tg.setCameraTarget(context => context[CustomNpcKeys.SlacksMudGolem]),
             ])
         ]),
         tg.textDialog(LocalizationKey.Script_1_Opening_1, ctx => ctx[CustomNpcKeys.SlacksMudGolem], 3),
@@ -51,10 +42,10 @@ const onStart = (complete: () => void) => {
         tg.textDialog(LocalizationKey.Script_1_Opening_7, ctx => ctx[CustomNpcKeys.SlacksMudGolem], 3),
         tg.textDialog(LocalizationKey.Script_1_Opening_8, ctx => ctx[CustomNpcKeys.SunsFanMudGolem], 3),
         tg.textDialog(LocalizationKey.Script_1_Opening_9, ctx => ctx[CustomNpcKeys.SlacksMudGolem], 3),
-        tg.panCameraLinear(ctx => ctx[CustomNpcKeys.SunsFanMudGolem].GetAbsOrigin(), Entities.FindAllByName("dota_badguys_fort")[0].GetAbsOrigin(), 4),
+        tg.panCameraLinear(ctx => ctx[CustomNpcKeys.SlacksMudGolem].GetAbsOrigin(), Entities.FindAllByName("dota_badguys_fort")[0].GetAbsOrigin(), 8),
         tg.textDialog(LocalizationKey.Script_1_Opening_10, ctx => ctx[CustomNpcKeys.SunsFanMudGolem], 3),
         tg.textDialog(LocalizationKey.Script_1_Opening_11, ctx => ctx[CustomNpcKeys.SlacksMudGolem], 3),
-        tg.panCameraExponential(Entities.FindAllByName("dota_badguys_fort")[0].GetAbsOrigin(), _ => playerHero.GetAbsOrigin(), 0.8),
+        tg.panCameraExponential(Entities.FindAllByName("dota_badguys_fort")[0].GetAbsOrigin(), _ => playerHero.GetAbsOrigin(), 0.5),
         tg.setCameraTarget(() => playerHero),
     ])
 
