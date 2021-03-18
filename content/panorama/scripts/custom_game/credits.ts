b82b13cc1a62e90872367518ef3e1dec6c16955d
Robin Kahlow <robinkahlow@web.de>
2021-03-18 02:02:33 +0000

    add portraits to credits view

--- content/panorama/scripts/custom_game/credits.ts
+++ content/panorama/scripts/custom_game/credits.ts
@@ -3,10 +3,42 @@ const nameLabel = $("#CreditsName") as LabelPanel;
 const descriptionLabel = $("#CreditsDescriptionLabel") as LabelPanel;
 const socialsContainer = $("#CreditsContainerContent");
 
+const portraits: Record<string, string> = {
+    [CustomNpcKeys.Tsunami]: "file://{images}/custom_game/credits/avatars/Tsunami.png",
+    [CustomNpcKeys.Coccia]: "file://{images}/custom_game/credits/avatars/alexander_coccia.png",
+    [CustomNpcKeys.BSJ]: "file://{images}/custom_game/credits/avatars/bsj.png",
+    [CustomNpcKeys.Bowie]: "file://{images}/custom_game/credits/avatars/d2bowie.png",
+    [CustomNpcKeys.DotaFromZero]: "file://{images}/custom_game/credits/avatars/dota_from_zero.png",
+    [CustomNpcKeys.DotaU]: "file://{images}/custom_game/credits/avatars/dota_university.png",
+    [CustomNpcKeys.Liquipedia]: "file://{images}/custom_game/credits/avatars/liquipedia.png",
+    [CustomNpcKeys.PurgePugna]: "file://{images}/custom_game/credits/avatars/purge.png",
+    [CustomNpcKeys.RedditDota]: "file://{images}/custom_game/credits/avatars/redditdota2.png",
+    [CustomNpcKeys.Flam3s]: "file://{images}/custom_game/credits/avatars/moddota.png",
+    [CustomNpcKeys.Perry]: "file://{images}/custom_game/credits/avatars/moddota.png",
+    [CustomNpcKeys.PongPing]: "file://{images}/custom_game/credits/avatars/moddota.png",
+    [CustomNpcKeys.Shush]: "file://{images}/custom_game/credits/avatars/moddota.png",
+    [CustomNpcKeys.SinZ]: "file://{images}/custom_game/credits/avatars/moddota.png",
+    [CustomNpcKeys.SmashTheState]: "file://{images}/custom_game/credits/avatars/moddota.png",
+    [CustomNpcKeys.Tora]: "file://{images}/custom_game/credits/avatars/moddota.png",
+    [CustomNpcKeys.Toyoka]: "file://{images}/custom_game/credits/avatars/moddota.png",
+    [CustomNpcKeys.VicFrank]: "file://{images}/custom_game/credits/avatars/moddota.png",
+    [CustomNpcKeys.Yoyo]: "file://{images}/custom_game/credits/avatars/moddota.png",
+    [CustomNpcKeys.Yoyo]: "file://{images}/custom_game/credits/avatars/moddota.png",
+};
+
+enum SocialType {
+    Twitter = "Twitter",
+    Youtube = "Youtube",
+    Discord = "Discord",
+    Website = "Website",
+}
+
 GameEvents.Subscribe("credits_interact", event => {
     // Set fields on interaction panel
-    // TODO: Use correct avatar based on event.name here?
-    avatarPanel.SetImage("file://{images}/custom_game/credits/avatars/Tsunami.png");
+    if (portraits[event.name]) {
+        avatarPanel.SetImage(portraits[event.name]);
+    }
+
     nameLabel.text = $.Localize(event.name);
     if (event.description) {
         descriptionLabel.text = $.Localize(event.description);
@@ -28,13 +60,6 @@ GameEvents.Subscribe("credits_interact_stop", () => {
     $.GetContextPanel().RemoveClass("Visible");
 });
 
-enum SocialType {
-    Twitter = "Twitter",
-    Youtube = "Youtube",
-    Discord = "Discord",
-    Website = "Website",
-}
-
 function addSocialIfExists(social: SocialType, unitName: string) {
     const linkLocalizationKey = `${unitName}_${social}`;
     const link = $.Localize(linkLocalizationKey);
@@ -44,7 +69,7 @@ function addSocialIfExists(social: SocialType, unitName: string) {
 
         const socialName = $.CreatePanel("Label", socialsContainer, "");
         socialName.AddClass("SocialMedia");
-        socialName.text = social;
+        socialName.text = link;
 
         // TODO: Manage rows/columns
         // TODO: Register link
