40583d0e55d29cb3f397213db066f76254510522
Robin Kahlow <robinkahlow@web.de>
2021-03-18 02:46:41 +0000

    more compact credits, handle cols, add url fn

--- content/panorama/scripts/custom_game/credits.ts
+++ content/panorama/scripts/custom_game/credits.ts
@@ -2,6 +2,8 @@ const avatarPanel = $("#Avatar") as ImagePanel;
 const nameLabel = $("#CreditsName") as LabelPanel;
 const descriptionLabel = $("#CreditsDescriptionLabel") as LabelPanel;
 const socialsContainer = $("#CreditsContainerContent");
+const socialsCol1 = $("#CreditsSocialsColumn1");
+const socialsCol2 = $("#CreditsSocialsColumn2");
 
 const portraits: Record<string, string> = {
     [CustomNpcKeys.Tsunami]: "file://{images}/custom_game/credits/avatars/Tsunami.png",
@@ -23,7 +25,6 @@ const portraits: Record<string, string> = {
     [CustomNpcKeys.Toyoka]: "file://{images}/custom_game/credits/avatars/moddota.png",
     [CustomNpcKeys.VicFrank]: "file://{images}/custom_game/credits/avatars/moddota.png",
     [CustomNpcKeys.Yoyo]: "file://{images}/custom_game/credits/avatars/moddota.png",
-    [CustomNpcKeys.Yoyo]: "file://{images}/custom_game/credits/avatars/moddota.png",
 };
 
 enum SocialType {
@@ -33,10 +34,29 @@ enum SocialType {
     Website = "Website",
 }
 
+function makeSocialUrl(name: string, socialType: SocialType) {
+    switch (socialType) {
+        case "Twitter":
+            return `https://twitter.com/${name}`;
+            break;
+        case "Youtube":
+            return `https://youtube.com/${name}`;
+            break;
+        case "Discord":
+            return `https://discord.gg/${name}`;
+            break;
+        case "Website":
+            return `https://${name}`;
+            break;
+    }
+}
+
 GameEvents.Subscribe("credits_interact", event => {
     // Set fields on interaction panel
     if (portraits[event.name]) {
         avatarPanel.SetImage(portraits[event.name]);
+    } else {
+        avatarPanel.SetImage("");
     }
 
     nameLabel.text = $.Localize(event.name);
@@ -46,11 +66,15 @@ GameEvents.Subscribe("credits_interact", event => {
         descriptionLabel.text = "";
     }
 
-    socialsContainer.RemoveAndDeleteChildren();
-    addSocialIfExists(SocialType.Twitter, event.name);
-    addSocialIfExists(SocialType.Youtube, event.name);
-    addSocialIfExists(SocialType.Discord, event.name);
-    addSocialIfExists(SocialType.Website, event.name);
+    socialsCol1.RemoveAndDeleteChildren();
+    socialsCol2.RemoveAndDeleteChildren();
+
+    let socialCount = 0;
+    for (const social of Object.values(SocialType)) {
+        if (addSocialIfExists(social, event.name, socialCount < 2 ? socialsCol1 : socialsCol2)) {
+            socialCount++;
+        }
+    }
 
     // Show interaction panel
     $.GetContextPanel().AddClass("Visible");
@@ -60,18 +84,24 @@ GameEvents.Subscribe("credits_interact_stop", () => {
     $.GetContextPanel().RemoveClass("Visible");
 });
 
-function addSocialIfExists(social: SocialType, unitName: string) {
+
+
+function addSocialIfExists(social: SocialType, unitName: string, container: Panel) {
     const linkLocalizationKey = `${unitName}_${social}`;
-    const link = $.Localize(linkLocalizationKey);
-    if (link && link !== linkLocalizationKey) {
-        const iconPanel = $.CreatePanel("Panel", socialsContainer, "");
+    const socialId = $.Localize(linkLocalizationKey);
+    if (socialId && socialId !== linkLocalizationKey) {
+        const iconPanel = $.CreatePanel("Panel", container, "");
         iconPanel.AddClass(social);
 
-        const socialName = $.CreatePanel("Label", socialsContainer, "");
+        const socialName = $.CreatePanel("Label", container, "");
         socialName.AddClass("SocialMedia");
-        socialName.text = link;
+        socialName.text = socialId;
+        socialName.hittest = true;
+        // TODO: Why does ExternalBrowserGoToURL not exist in ts defs?
+        // socialName.SetPanelEvent("onactivate", () => ExternalBrowserGoToURL(makeSocialUrl(socialId, social)));
 
-        // TODO: Manage rows/columns
-        // TODO: Register link
+        return true;
     }
+
+    return false;
 }
