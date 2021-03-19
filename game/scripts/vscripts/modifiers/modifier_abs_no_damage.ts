fc2dfd44d8372923c864eb9637569d8263ee4cb7
Shush <almogosheriko@gmail.com>
2021-03-20 00:12:34 +0200

    Update modifier_abs_no_damage.ts

--- game/scripts/vscripts/modifiers/modifier_abs_no_damage.ts
+++ game/scripts/vscripts/modifiers/modifier_abs_no_damage.ts
@@ -3,17 +3,15 @@ import { BaseModifier, registerModifier } from "../lib/dota_ts_adapter"
 @registerModifier()
 export class modifier_abs_no_damage extends BaseModifier {
 
-    IsHidden() {return true}
-    IsDebuff() {return false}
-    IsPurgable() {return false}
+    IsHidden() { return true }
+    IsDebuff() { return false }
+    IsPurgable() { return false }
 
-    DeclareFunctions(): ModifierFunction[]
-    {
+    DeclareFunctions(): ModifierFunction[] {
         return [ModifierFunction.INCOMING_DAMAGE_PERCENTAGE]
     }
 
-    GetModifierIncomingDamage_Percentage(): number
-    {
+    GetModifierIncomingDamage_Percentage(): number {
         return -100
     }
 }
