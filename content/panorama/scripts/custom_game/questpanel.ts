bd890a295407693a52bf8ff1d5e1b8615cdb7826
Robin Kahlow <robinkahlow@web.de>
2021-03-24 20:31:37 +0000

    detect goal changes, highlight active and changed-text goals

--- content/panorama/scripts/custom_game/questpanel.ts
+++ content/panorama/scripts/custom_game/questpanel.ts
@@ -13,23 +13,91 @@ function parseServerArray<T>(array: { [key: string]: T }): T[] {
     return result;
 }
 
+const goalRows: GoalRow[] = []
+
+type GoalRow = {
+    label: LabelPanel
+    bulletPoint: Panel
+    rowPanel: Panel
+    goal: NetworkedData<Goal>
+}
+
+function createGoalRow(questPanel: Panel, goal: NetworkedData<Goal>): GoalRow {
+    const localizeInline = GameUI.CustomUIConfig().localizeInline;
+
+    const rowPanel = $.CreatePanel("Panel", questPanel, "");
+    rowPanel.AddClass("Row");
+
+    const bulletPoint = $.CreatePanel("Panel", rowPanel, "");
+    bulletPoint.AddClass(goal.completed ? "QuestsCompleted" : "QuestsUncompleted");
+
+    const label = $.CreatePanel("Label", rowPanel, "");
+    label.AddClass(goal.completed ? "QuestCompletedText" : "QuestUncompletedText");
+    label.text = localizeInline(goal.text);
+
+    label.AddClass("TextGlow");
+
+    return {
+        bulletPoint,
+        label,
+        rowPanel,
+        goal,
+    }
+}
+
 GameEvents.Subscribe("set_goals", event => {
     const localizeInline = GameUI.CustomUIConfig().localizeInline;
 
-    // Clear the goals list and add each goal to the list.
     const questPanel = $("#QuestsPanelContainer");
-    questPanel.RemoveAndDeleteChildren();
 
-    // TODO: Should we make sure that the goals are sorted by index?
-    for (const goal of parseServerArray(event.goals)) {
-        const rowPanel = $.CreatePanel("Panel", questPanel, "");
-        rowPanel.AddClass("Row");
+    const newGoals = parseServerArray(event.goals);
 
-        const bulletPoint = $.CreatePanel("Panel", rowPanel, "");
-        bulletPoint.AddClass(goal.completed ? "QuestsCompleted" : "QuestsUncompleted");
+    // Add or remove new rows as required
+    if (newGoals.length > goalRows.length) {
+        for (let i = goalRows.length; i < newGoals.length; i++) {
+            goalRows.push(createGoalRow(questPanel, newGoals[i]));
+        }
+    } else if (newGoals.length < goalRows.length) {
+        for (let i = goalRows.length - 1; i >= newGoals.length; i--) {
+            goalRows[i].rowPanel.DeleteAsync(0);
+        }
 
-        const label = $.CreatePanel("Label", rowPanel, "");
-        label.AddClass(goal.completed ? "QuestCompletedText" : "QuestUncompletedText");
-        label.text = localizeInline(goal.text);
+        goalRows.splice(newGoals.length);
+    }
+
+    // Update the rows
+    for (let i = 0; i < newGoals.length; i++) {
+        const newGoal = newGoals[i];
+        const currentGoal = goalRows[i].goal
+
+        let shouldHighlight = false;
+
+        // Check if the goal at this index changed
+
+        if (currentGoal.completed !== newGoal.completed) {
+            goalRows[i].bulletPoint.RemoveClass(currentGoal.completed ? "QuestsCompleted" : "QuestsUncompleted");
+            goalRows[i].label.RemoveClass(currentGoal.completed ? "QuestCompletedText" : "QuestUncompletedText");
+            goalRows[i].bulletPoint.AddClass(newGoal.completed ? "QuestsCompleted" : "QuestsUncompleted");
+            goalRows[i].label.AddClass(newGoal.completed ? "QuestCompletedText" : "QuestUncompletedText");
+
+            // Highlight newly active goals
+            if (!newGoal.completed) {
+                shouldHighlight = true;
+            }
+        }
+
+        if (goalRows[i].goal.text !== newGoal.text) {
+            goalRows[i].label.text = localizeInline(newGoal.text);
+            // Highlight changed goals
+            shouldHighlight = true;
+        }
+
+        // Add an animation if desired
+        if (shouldHighlight) {
+            goalRows[i].label.RemoveClass("TextGlow");
+            goalRows[i].label.AddClass("TextGlow");
+        }
+
+        goalRows[i].goal = newGoal
     }
 });
