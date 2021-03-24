a13757ca1e89dfa1a7947ecbec3b8145622f43a9
Robin Kahlow <robinkahlow@web.de>
2021-03-24 19:52:31 +0000

    Add current chapter label (#546)
    * add current chapter label
    
    * add change animation
    
    * fix text not being bold

--- content/panorama/scripts/custom_game/hud.ts
+++ content/panorama/scripts/custom_game/hud.ts
@@ -291,3 +291,13 @@ function onShowSkipChapter3Button(show: boolean) {
 }
 
 GameEvents.Subscribe("show_chapter3_skip_button", event => onShowSkipChapter3Button(event.show !== 0));
+
+// Current chapter display
+
+GameEvents.Subscribe("section_started", event => {
+    const chapterName = event.section.split("_")[0];
+    const currentChapterLabel = $("#CurrentChapter") as LabelPanel;
+    currentChapterLabel.text = $.Localize(`#Chapter_${chapterName.substr(chapterName.length - 1)}`).toLocaleUpperCase();
+    currentChapterLabel.RemoveClass("TextGlow");
+    currentChapterLabel.AddClass("TextGlow");
+});
