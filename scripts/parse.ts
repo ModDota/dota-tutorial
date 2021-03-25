b177f7fd874025e8e5e18f19e64dfc1be7663841
Robin Kahlow <robinkahlow@web.de>
2021-03-25 23:43:30 +0000

    dont write garbage localization csv (#563)

--- scripts/parse.ts
+++ scripts/parse.ts
@@ -12,10 +12,34 @@ const root = addon_english["lang"];
 const language = root["Language"];
 const tokens = root["Tokens"];
 
+function shouldWrite(key: string) {
+    return (!key.endsWith("_titles_name") &&
+        !key.endsWith("_titles_num") &&
+        !key.endsWith("_twitter") &&
+        !key.endsWith("_twitch") &&
+        !key.endsWith("_discord") &&
+        !key.endsWith("_website") &&
+        !key.endsWith("_youtube") &&
+        !((key.startsWith("npc_dota_hero_") || key.startsWith("npc_mud_golem_")) && !key.includes("_titles_")) &&
+        !key.match(/npc_dota_tutorial_[a-zA-Z]+/)) || (
+            key === "npc_dota_tutorial_translators" ||
+            key === "npc_dota_tutorial_indiegogo" ||
+            key === "npc_dota_tutorial_target_dummy" ||
+            key === "npc_dota_tutorial_radiant_melee_creep" ||
+            key === "npc_dota_tutorial_radiant_ranged_creep" ||
+            key === "npc_dota_tutorial_dire_melee_creep" ||
+            key === "npc_dota_tutorial_dire_ranged_creep" ||
+            key === "npc_dota_tutorial_translators_titles_name" ||
+            key === "npc_dota_tutorial_indiegogo_titles_name"
+        );
+}
+
 // Serialize tokens to translation csv
 const lines = [`"String ID","Source text","Translation"`];
 for (const [k, v] of Object.entries(tokens)) {
-    lines.push(`"${k}","${v}",""`);
+    if (shouldWrite(k)) {
+        lines.push(`"${k}","${v}",""`);
+    }
 }
 
 // Write csv to outPath
